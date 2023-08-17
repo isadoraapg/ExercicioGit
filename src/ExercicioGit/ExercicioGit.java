6a72d31d56f5fe323d4d2da609e8df92f668d355
Isadora <isadorapgalego@gmail.com>
2023-08-17 17:03:05 -0300

    Exercicio Git

---
+++ src/ExercicioGit/ExercicioGit.java
@@ -0,0 +1,37 @@
+package ExercicioGit;
+
+import java.util.Scanner;
+
+public class ExercicioGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N1, N2, N3, N4, N5, soma;
+		
+		System.out.println("Digite 5 notas: ");
+		N1 = sc.nextInt();
+		N2 = sc.nextInt();
+		N3 = sc.nextInt();
+		N4 = sc.nextInt();
+		N5 = sc.nextInt();
+		
+		soma = N1 + N2 + N3 + N4 + N5;
+		
+		System.out.println("A soma das notas é: " + soma);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
